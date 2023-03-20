e6d62490dd5046ff707e6c80d3aa95f9e4964bd3
Mariusz Trzeciak <mariusz.trzeciak09@gmail.com>
2023-03-20 18:59:00 +0100

    Code cleanup

---
+++ AWWW_lab2_gr5/Controllers/LeagueController.cs
@@ -0,0 +1,36 @@
+﻿using AWWW_lab2_gr5.Data;
+using AWWW_lab2_gr5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWWW_lab2_gr5.Controllers
+{
+    public class LeagueController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeagueController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<League> objLeagueList = _db.Leagues;
+            return View(objLeagueList);
+        }
+        
+        // GET
+        public IActionResult AddNewLeague() 
+        {
+            return View();
+        }
+
+        // POST
+        public IActionResult AddNewLeague(League obj)
+        {
+            _db.Leagues.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
