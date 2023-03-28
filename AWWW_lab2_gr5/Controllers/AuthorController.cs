d5717cf1ae47a0646d7dd46972bfe74a40bf715c
Mariusz Trzeciak <mariusz.trzeciak09@gmail.com>
2023-03-28 23:30:11 +0200

    Add create and delete method and views for them

--- AWWW_lab2_gr5/Controllers/AuthorController.cs
+++ AWWW_lab2_gr5/Controllers/AuthorController.cs
@@ -1,6 +1,7 @@
 ﻿using AWWW_lab2_gr5.Data;
 using AWWW_lab2_gr5.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AWWW_lab2_gr5.Controllers
 {
@@ -33,6 +34,66 @@ namespace AWWW_lab2_gr5.Controllers
                 _db.Authors.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
+        }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var authorFromDb = _db.Authors.FirstOrDefault(a => a.Id == id);
+
+            if (authorFromDb == null || authorFromDb.Id == 0) 
+            {
+                return NotFound();
+            }
+            return View(authorFromDb);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Author obj)
+        {
+            _db.Authors.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var authorFromDb = _db.Authors.FirstOrDefault(a => a.Id == id);
+
+            if (authorFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(authorFromDb);
+        }
+
+        // POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Authors.FirstOrDefault(x => x.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Authors.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
