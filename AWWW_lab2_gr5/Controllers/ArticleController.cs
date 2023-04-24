ea834966d9198f9ec9152b469c7c0ba4f42517b0
Mariusz Trzeciak <mariusz.trzeciak09@gmail.com>
2023-04-24 22:34:35 +0200

    Add part of view and controller for Article class

--- AWWW_lab2_gr5/Controllers/ArticleController.cs
+++ AWWW_lab2_gr5/Controllers/ArticleController.cs
@@ -1,5 +1,7 @@
 ﻿using AWWW_lab2_gr5.Data;
+using AWWW_lab2_gr5.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace AWWW_lab2_gr5.Controllers
@@ -41,6 +43,31 @@ namespace AWWW_lab2_gr5.Controllers
             {
                 return NotFound();
             }
+
+            return View(article);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Articles
+                .Include(a => a.Author)
+                .Include(a => a.Category)
+                .Include(a => a.Tags)
+                .Include(a => a.Comments)
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.AuthorList = new SelectList(await _context.Authors.ToListAsync(), "Id", "GetFullName", article.AuthorId);
+            ViewBag.CategoryList = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name", article.CategoryId);
 
             return View(article);
         }
